ef77f33aa73dc25144252c7dc54d66090e5f5faa
TheBenKnee <therealbenknee@gmail.com>
2023-03-03 12:19:32 -0800

    Initial Push
    Dood

---
+++ Assets/Scripts/Dood.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dood : MonoBehaviour
+{
+    public int doodness = 420;
+
+    public void BeADood()
+    {
+        Debug.Log("Im a dood");
+    }
+}
